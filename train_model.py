ec202c3a483ccb60615dbe6a3cdaa557546ab150
tirth1309 <57043695+tirth1309@users.noreply.github.com>
2024-03-15 00:41:49 +0530

    Update train_model.py adding call functions

--- train_model.py
+++ train_model.py
@@ -7,19 +7,19 @@ from utility import pipeline
 from mlflow.models import infer_signature
 
 # TODO: Set tht MLFlow tracking server uri
-uri = _________
+uri = "http://127.0.0.1:6001/"
 # Use mlflow.set_tracking_uri to set the uri
 mlflow.set_tracking_uri(uri)
 
 # Set experiment name
-email="______@andrew.cmu.edu" # TODO: Use a customized experiment name
+email="tirthvip@andrew.cmu.edu" # TODO: Use a customized experiment name
 experiment_name=f"{email}-lab7"
 mlflow.set_experiment(experiment_name)
 
 # Begin the model training and evaluation process
 
 # TODO: Generates train and test dataset using `pipeline.data_preprocessing` function
-X_train,X_test,y_train,y_test= ________
+X_train,X_test,y_train,y_test= data_preprocessing()
 
 # Train the model:
 params = {
@@ -30,10 +30,10 @@ params = {
 }
 
 # TODO: Use `pipeline.train_logistic_regression` to generate trained model
-trained_model=__________________
+trained_model=train_logistic_regression(X_train, y_train, params)
 
 # TODO: use `pipeline.evaluation` to evaluate the model
-accuracy=_____________________
+accuracy=evaluation(training_model, X_test, y_test)
 
 # Log model and metrics to tracking serverhost>
 # Start an MLflow run
